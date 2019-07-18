0b577a4c4ae9585c97e0e972e1fd2dc14915d041
Bigfish-Alex <632388230@qq.com>
2019-07-18 17:48:40 +0800

    增加登陆页面，增加sass-loader、node-sass依赖

--- backstage-v3.0/src/components/login/login.vue
+++ backstage-v3.0/src/components/login/login.vue
@@ -1,35 +1,78 @@
 <template>
-  <div class="login">
-    <router-link to="/index">首页</router-link>  <br>
-    <router-link to="/login">登陆</router-link>  <br>
-    {{name}}
+  <div class="login" id="login_container">
+
+    <div id="particles">
+    </div>
+
+    <div class="login_form_container">
+        <h1>BIGFISH后台管理系统</h1>
+
+        <div class="input_container">
+            <input type="text" v-model="username" @input="listenUsernameInput" placeholder="用户名">
+            <p class="warning" v-if="isShowUsernameWarn">*请输入用户名</p>
+        </div>
+
+        <div class="input_container">
+            <input type="password" v-model="password" @input="listenPasswordInput" placeholder="密码">
+            <p class="warning" v-if="isShowPasswordWarn">*请输入密码</p>
+        </div>
+
+        <button @click="login">
+            登陆
+        </button>
+    </div>
+
+    <p class="copyright">@copyright BIGFISH后台管理系统 FROM 2019-2022 </p>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      name:'this is Login!'
-    }
+  data() {
+    return {
+      isShowUsernameWarn:false,
+      isShowPasswordWarn:false,
+      username:'',
+      password:'',
+    };
+  },
+  methods:{
+      login(){
+          if( this.username.length<=0 ){
+              this.isShowUsernameWarn=true;
+              return false;
+          }
+          if( this.password.length<=0 ){
+              this.isShowPasswordWarn=true;
+              return false;
+          }
+          this.$router.push({path:'index'});
+      },
+      listenUsernameInput(){
+          if( this.username.length<=0 ){
+              this.isShowUsernameWarn=true;
+          }else{
+              this.isShowUsernameWarn=false;
+          }
+      },
+      listenPasswordInput(){
+          if( this.password.length<=0 ){
+              this.isShowPasswordWarn=true;
+          }else{
+              this.isShowPasswordWarn=false;
+          }
+      }
+  },
+  mounted: function() {
+      $("#particles").particleground({
+        dotColor: "#5cbdaa",
+        lineColor: "#5cbdaa"
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+    @import "./login.scss"
 </style>
